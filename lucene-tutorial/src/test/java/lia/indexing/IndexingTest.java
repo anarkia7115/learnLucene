919f07b83ab84ec1e2c56b929593b768dca6f11c
shawn <shawn24601@gmail.com>
2016-12-28 18:42:34 +0800

    add annotations

--- lucene-tutorial/src/test/java/lia/indexing/IndexingTest.java
+++ lucene-tutorial/src/test/java/lia/indexing/IndexingTest.java
@@ -8,13 +8,17 @@ import org.apache.lucene.analysis.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.FieldSelectorResult;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermEnum;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import lia.common.TestUtil;
@@ -28,7 +32,9 @@ public class IndexingTest {
 	
 	private Directory directory;
 	
-	protected void setUp() throws Exception {
+	//@BeforeClass
+	@Before
+	public void setUp() throws Exception {
 		directory = new RAMDirectory();
 		
 		IndexWriter writer = getWriter();
@@ -57,6 +63,11 @@ public class IndexingTest {
 							   IndexWriter.MaxFieldLength.UNLIMITED);
 	}
 	
+	private IndexReader getReader() throws IOException {
+
+		return IndexReader.open(directory);
+	}
+	
 	protected int getHitCount(String fieldName, String searchString)
 	  throws IOException {
 		IndexSearcher searcher = new IndexSearcher(directory);
@@ -67,11 +78,87 @@ public class IndexingTest {
 		return hitCount;
 	}
 	
+	@Test
 	public void testIndexWriter() throws IOException {
 		IndexWriter writer = getWriter();
+		assertEquals(ids.length, writer.numDocs());
+		System.out.println("ids.length: " + Integer.toString(ids.length));
+		writer.close();
 	}
 	
 	@Test
+	public void testIndexReader() throws IOException {
+		IndexReader reader = IndexReader.open(directory);
+		System.out.println("ids.length: " + Integer.toString(ids.length));
+		assertEquals(ids.length,  reader.maxDoc());
+		assertEquals(ids.length,  reader.numDocs());
+		reader.close();
+	}
+	
+	//@Test
+	public void testDeleteBeforeOptimize() throws IOException {
+		IndexWriter writer = getWriter();
+		assertEquals(2,  writer.numDocs());
+		writer.deleteDocuments(new Term("id", "1"));
+		writer.commit();
+		assertTrue(writer.hasDeletions());
+		assertEquals(2, writer.maxDoc());
+		assertEquals(1, writer.numDocs());
+		writer.close();
+	}
+	
+	//@Test
+	public void testDeleteAfterOptimize() throws IOException {
+		IndexWriter writer = getWriter();
+		//System.out.println(writer.getInfoStream());
+		assertEquals(2, writer.numDocs());
+		writer.deleteDocuments(new Term("id", "1"));
+		writer.optimize();
+		writer.commit();
+		assertFalse(writer.hasDeletions());
+		assertEquals(1, writer.maxDoc());
+		assertEquals(1, writer.numDocs());
+		writer.close();
+	}
+	
+    @Test
+	public void testUpdate() throws IOException {
+		
+		assertEquals(1, getHitCount("city", "Amsterdam"));
+		
+		IndexWriter writer = getWriter();
+		System.out.println("numDocs" + Integer.toString(writer.numDocs()));
+		
+		Document doc = new Document();
+		doc.add(new Field("id", "1", 
+						  Field.Store.YES,
+						  Field.Index.NOT_ANALYZED));
+        doc.add(new Field("country", "Netherlands",
+                          Field.Store.YES,
+                          Field.Index.NO));
+        doc.add(new Field("contents", 
+                          "Den Haag has a lot of museums",
+                          Field.Store.NO,
+                          Field.Index.ANALYZED));
+        doc.add(new Field("city", "Den Haag",
+                          Field.Store.YES,
+                          Field.Index.NOT_ANALYZED));
+        writer.updateDocument(new Term("id", "1"), 
+                              doc);
+        writer.close();
+		System.out.println("numDocs: " + Integer.toString(writer.numDocs()));
+		IndexReader reader = getReader();
+		TermEnum termEnum = reader.terms();
+		while(termEnum.next()) {
+			Term term = termEnum.term();
+			System.out.println(term.text());
+		}
+		System.out.println("reader terms: " + reader.terms());
+		assertEquals(0, getHitCount("city", "Amsterdam"));
+		assertEquals(1, getHitCount("city", "Den Haag"));
+	}
+	
+	//@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
